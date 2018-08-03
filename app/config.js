4fd4ba666e0dba9c0c642fc1bc3f2f471b198af0
Lisa Rae <lisa@scrumconnect.com>
2018-08-03 11:34:35 +0100

    First commit

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Winter Fuel Payments',
+  serviceName: 'Winter Fuel Payment',
 
   // Default port that prototype runs on
   port: '3000',
