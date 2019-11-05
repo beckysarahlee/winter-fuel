c74e2167991423256dbcb7b587645083913ce6cf
Becky <beckylee.digitaldwp@gmail.com>
2019-11-05 11:24:01 +0000

    Returned payments and hospital record

--- app/routes/sprint27c.js
+++ app/routes/sprint27c.js
@@ -9,7 +9,6 @@ router.use((req, res, next) => {
   })
 
   // Query
-
   router.post('/sprint27c/query', function(req, res) {
   if ( req.body['query'] === 'eligibility' ) {
     res.redirect('date-of-birth');
@@ -19,138 +18,33 @@ router.use((req, res, next) => {
   });
 
 
+  // FIND SOMEONE AND SECURITY -------------------------------------------------
 
-  router.post('/sprint27c/frequency', (req, res) => {
-    res.redirect('/sprint27c/payment')
-  })
-  ;
-
-  router.use((req, res, next) => {
-    if (req.method === 'POST') {
-      console.log(JSON.stringify(req.session.data, null, 2))
-    }
-    next()
-  })
-
-  router.post('/sprint27c/bankdetails', (req, res) => {
-    res.redirect('/sprint27c/reissue-payment')
-  })
-  ;
-
-  router.use((req, res, next) => {
-    if (req.method === 'POST') {
-      console.log(JSON.stringify(req.session.data, null, 2))
-    }
-    next()
-  })
-
-  router.post('/sprint27c/workphone', (req, res) => {
-    res.redirect('/sprint27c/overview')
-  })
-  ;
-
-  router.use((req, res, next) => {
-    if (req.method === 'POST') {
-      console.log(JSON.stringify(req.session.data, null, 2))
-    }
-    next()
-  })
-
-  router.post('/sprint27c/mobilephone', (req, res) => {
-    res.redirect('/sprint27c/contact')
-  })
-  ;
-
-  router.use((req, res, next) => {
-    if (req.method === 'POST') {
-      console.log(JSON.stringify(req.session.data, null, 2))
-    }
-    next()
-  })
-
-  router.post('/sprint27c/email', (req, res) => {
-    res.redirect('/sprint27c/contact')
-  })
-  ;
-
-  router.use((req, res, next) => {
-    if (req.method === 'POST') {
-      console.log(JSON.stringify(req.session.data, null, 2))
-    }
-    next()
-  })
-
-  router.post('/sprint27c/homephone', (req, res) => {
-    res.redirect('/sprint27c/contact')
-  })
-  ;
-
-  router.use((req, res, next) => {
-    if (req.method === 'POST') {
-      console.log(JSON.stringify(req.session.data, null, 2))
-    }
-    next()
-  })
-
+  // Find someone to find result
   router.post('/sprint27c/find', (req, res) => {
     res.redirect('/sprint27c/find-1')
   })
   ;
 
+  // Find result to security
   router.post('/sprint27c/find-1', (req, res) => {
     res.redirect('/sprint27c/security')
   })
   ;
 
-
+  // Security into record with security confirmation box
   router.post('/sprint27c/security', (req, res) => {
     res.redirect('/sprint27c/overview-security')
   })
   ;
 
+  // CONTACT CHANGES -----------------------------------------------------------
 
-
-  router.use((req, res, next) => {
-    if (req.method === 'POST') {
-      console.log(JSON.stringify(req.session.data, null, 2))
-    }
-    next()
-  })
-
-  router.post('/sprint27c/address', (req, res) => {
-    res.redirect('/sprint27c/address-1')
-  })
-  ;
-
-  router.post('/sprint27c/address-1', (req, res) => {
-    res.redirect('/sprint27c/living-with')
-  })
-  ;
-
-
-  // Living with
-  router.post('/sprint27c/living-with', function(req, res) {
-    if ( req.body['living-with'] === 'Yes' ) {
-      res.redirect('living-with-age');
-    } else {
-      res.redirect('homephone-address');
-    }
-  });
-
-  // Living with age
-  router.post('/sprint27c/living-with-age', (req, res) => {
+  // Home phone number change back to contact
+  router.post('/sprint27c/homephone', (req, res) => {
     res.redirect('/sprint27c/contact')
   })
   ;
-
-  // Change of address and home phone number
-  router.post('/sprint27c/homephone-address', function(req, res) {
-    if ( req.body['homephone-address'] === 'Yes' ) {
-      res.redirect('homephone');
-    } else {
-      res.redirect('contact');
-    }
-  });
 
   // Home phone number removal
   router.post('/sprint27c/homephone-remove', function(req, res) {
@@ -161,6 +55,26 @@ router.use((req, res, next) => {
     }
   });
 
+  // Work phone number change back to contact
+  router.post('/sprint27c/workphone', (req, res) => {
+    res.redirect('/sprint27c/contact')
+  })
+  ;
+
+  // Work phone number removal
+  router.post('/sprint27c/workphone-remove', function(req, res) {
+    if ( req.body['workphone-remove'] === 'Yes' ) {
+      res.redirect('contact');
+    } else {
+      res.redirect('workphone');
+    }
+  });
+
+  // Mobile phone number change back to contact
+  router.post('/sprint27c/mobilephone', (req, res) => {
+    res.redirect('/sprint27c/contact')
+  })
+  ;
 
   // Mobile phone number removal
   router.post('/sprint27c/mobilephone-remove', function(req, res) {
@@ -171,16 +85,8 @@ router.use((req, res, next) => {
     }
   });
 
-  // Work phone number removal & change
-  router.post('/sprint27c/workphone-remove', function(req, res) {
-    if ( req.body['workphone-remove'] === 'Yes' ) {
-      res.redirect('contact');
-    } else {
-      res.redirect('workphone');
-    }
-  });
-
-  router.post('/sprint27c/workphone', (req, res) => {
+  // Email address change back to contact
+  router.post('/sprint27c/email', (req, res) => {
     res.redirect('/sprint27c/contact')
   })
   ;
@@ -194,56 +100,60 @@ router.use((req, res, next) => {
     }
   });
 
-  // Marital status change
-  router.post('/sprint27c/marital-status', (req, res) => {
-    res.redirect('/sprint27c/marriage-details')
-  })
-  ;
-
-  router.post('/sprint27c/marriage-details', (req, res) => {
-    res.redirect('/sprint27c/marriage-certificate')
-  })
-  ;
-
-  // Contact preferences
+  // Contact preference change back to contact
   router.post('/sprint27c/contact-preferences', (req, res) => {
     res.redirect('/sprint27c/contact')
   })
   ;
 
-  // Removing occupants, underpayment
-  router.post('/sprint27c/occupants', (req, res) => {
-    res.redirect('/sprint27c/reason-removed1')
+  // Address -----------------------------
+
+  router.post('/sprint27c/address', (req, res) => {
+    res.redirect('/sprint27c/address-1')
   })
   ;
 
-  router.post('/sprint27c/reason-removed1', (req, res) => {
-    res.redirect('/sprint27c/move-date')
+  router.post('/sprint27c/address-1', (req, res) => {
+    res.redirect('/sprint27c/living-with')
   })
   ;
 
-  router.post('/sprint27c/move-date', (req, res) => {
-    res.redirect('/sprint27c/reason-removed2')
+  // Change of address and home phone number
+  router.post('/sprint27c/homephone-address', function(req, res) {
+    if ( req.body['homephone-address'] === 'Yes' ) {
+      res.redirect('homephone');
+    } else {
+      res.redirect('contact');
+    }
+  });
+
+  // Living with anyone at address change
+  router.post('/sprint27c/living-with', function(req, res) {
+    if ( req.body['living-with'] === 'Yes' ) {
+      res.redirect('living-with-age');
+    } else {
+      res.redirect('homephone-address');
+    }
+  });
+
+  // Living with age back to contact
+  router.post('/sprint27c/living-with-age', (req, res) => {
+    res.redirect('/sprint27c/contact')
   })
   ;
 
-  router.post('/sprint27c/reason-removed2', (req, res) => {
-    res.redirect('/sprint27c/check-underpayment')
+
+  // PAYMENT CHANGES -----------------------------------------------------------
+
+  // Bank details to reissue payment
+  router.post('/sprint27c/bankdetails', (req, res) => {
+    res.redirect('/sprint27c/reissue-payment')
   })
   ;
 
-  router.post('/sprint27c/check-underpayment', (req, res) => {
-    res.redirect('/sprint27c/evidence-request')
-  })
-  ;
-
-  router.post('/sprint27c/evidence-request', (req, res) => {
-    res.redirect('/sprint27c/overview-awaiting-verification')
-  })
-  ;
-
-  router.post('/sprint27c/evidence-verification', (req, res) => {
-    res.redirect('/sprint27c/overview-evidence-received')
+  // Reissue payment to payment with confirmation and updated
+  router.post('/sprint27c/reissue-payment', (req, res) => {
+    res.redirect('/sprint27c/payment-confirmation')
   })
   ;
 
