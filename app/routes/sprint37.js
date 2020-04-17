760b24c0813261fd44fceb3f033d25c7c2f8e126
Becky <beckylee.digitaldwp@gmail.com>
2020-04-17 16:01:36 +0100

    Updates to international journey

--- app/routes/sprint37.js
+++ app/routes/sprint37.js
@@ -9,11 +9,27 @@ router.use((req, res, next) => {
   })
 
 
-  // Teleclaim or postal claim
+  // Teleclaim or postal claim to find
   router.post('/sprint37-international/application-type', (req, res) => {
-    res.redirect('/sprint37-international/date-of-birth')
+    res.redirect('/sprint37-international/find')
   })
   ;
+
+  // Find to name
+  router.post('/sprint37-international/find', (req, res) => {
+    res.redirect('/sprint37-international/confirm-name')
+  })
+  ;
+
+  // Name to address
+  router.post('/sprint37-international/confirm-name', function(req, res) {
+      if (req.session.data["nino"] === "AB 12 34 57") {
+        res.redirect('international-record');
+      }
+       else {
+        res.redirect('date-of-birth');
+      }
+  });
 
   // DOB to phone/postal journey
   router.post('/sprint37-international/date-of-birth', function(req, res) {
@@ -23,30 +39,15 @@ router.use((req, res, next) => {
       else if (req.body["dob-year"] === "1959") {
         res.redirect('phone-too-young');
       } else {
-        res.redirect('find');
+        res.redirect('address');
       }
   });
-
-  // Find to name
-  router.post('/sprint37-international/find', (req, res) => {
-    res.redirect('/sprint37-international/confirm-name')
-  })
-  ;
-
-  // Name to address
-  router.post('/sprint37-international/confirm-name', (req, res) => {
-    res.redirect('/sprint37-international/address')
-  })
-  ;
 
   // Address during Q week
-  router.post('/sprint37-international/address', function(req, res) {
-      if (req.body["living-q-week"] === "yes") {
-        res.redirect('move-date');
-      } else {
-        res.redirect('address-q-week');
-      }
-  });
+  router.post('/sprint37-international/address', (req, res) => {
+    res.redirect('/sprint37-international/move-date')
+  })
+  ;
 
   // Name to address
   router.post('/sprint37-international/move-date', (req, res) => {
@@ -58,17 +59,22 @@ router.use((req, res, next) => {
   router.post('/sprint37-international/living-with', function(req, res) {
       if (req.body["living-with"] === "yes") {
         res.redirect('living-with-about');
+      }
+        else if (req.session.data["living-q-week"] === "no") {
+          res.redirect('address-q-week');
       } else {
         res.redirect('contact');
       }
   });
 
   // Living with about
-  router.post('/sprint37-international/living-with-about', (req, res) => {
-    res.redirect('/sprint37-international/contact')
-  })
-  ;
-
+  router.post('/sprint37-international/living-with-about', function(req, res) {
+      if (req.session.data["living-q-week"] === "no") {
+          res.redirect('address-q-week');
+      } else {
+        res.redirect('contact');
+      }
+  });
 
   // Contact to type of account
   router.post('/sprint37-international/contact', (req, res) => {
