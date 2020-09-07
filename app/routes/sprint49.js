67d7492ed415668453bc542616b3fad8118e10e5
Becky <beckylee.digitaldwp@gmail.com>
2020-09-07 11:40:45 +0100

    Changes

--- app/routes/sprint49.js
+++ app/routes/sprint49.js
@@ -369,10 +369,10 @@ router.post('/sprint49/change-payment-status', function(req, res) {
 
 // STOP PAYMENTS - OPTED OUT ---------------------------------------------------------
 router.post('/sprint49/stop-payments', function(req, res) {
-  if (req.body['stop-payments'] === 'dead') {
-    res.redirect('dead-stop');
+  if (req.body['stop-payments'] === 'yes') {
+    res.redirect('payment-opted-out');
   } else {
-    res.redirect('payment-opted-out-1');
+    res.redirect('payment');
   }
 });
 
